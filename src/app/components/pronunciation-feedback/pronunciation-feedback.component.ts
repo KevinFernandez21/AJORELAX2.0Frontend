cd3183a4684df303cc195ac57f34f337bc274512
KevinFernandez21 <kdfernan@espol.edu.ec>
2025-03-30 22:06:06 -0500

    first commit

---
+++ src/app/components/pronunciation-feedback/pronunciation-feedback.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pronunciation-feedback',
+  imports: [],
+  templateUrl: './pronunciation-feedback.component.html',
+  styleUrl: './pronunciation-feedback.component.css'
+})
+export class PronunciationFeedbackComponent {
+
+}
---
+++ src/app/components/word-details/word-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-details',
+  imports: [],
+  templateUrl: './word-details.component.html',
+  styleUrl: './word-details.component.css'
+})
+export class WordDetailsComponent {
+
+}
